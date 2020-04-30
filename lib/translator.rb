1c1fcea3ff9a6f8da04d49d2de2ba69ade1454a2
Felix Rodriguez <frod25@gmail.com>
2020-04-30 16:14:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,8 @@
 require 'pry'
 require 'yaml'
 
-def load_library
-  emoticons = YAML.load_file('emoticons.yml')
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
 end
 
 def get_japanese_emoticon
